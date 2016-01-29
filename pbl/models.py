7876c66229a896397da14f9c74771a3590fed901
django user <sujit.shrestha421@gmail.com>
2016-01-28 20:18:27 -0500

    syntax correction

--- pbl/models.py
+++ pbl/models.py
@@ -14,7 +14,7 @@ class Level(models.Model):
         self.published_date = timezone.now()
         self.save()
     def __str__(self):
-	return self.title
+        return self.title
 
 class Student(models.Model):
     first_name = models.CharField(max_length=100)
