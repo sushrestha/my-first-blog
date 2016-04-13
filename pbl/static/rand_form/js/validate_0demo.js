1703aeeeb805fcab35a786d3e4c57a07459103fb
bwillis2 <bwillis2@memphis.edu>
2016-04-13 16:31:46 -0500

    My bad on the demo

--- pbl/static/rand_form/js/validate_0demo.js
+++ pbl/static/rand_form/js/validate_0demo.js
@@ -48,13 +48,13 @@ function names()
 	{
 		result = result.concat("First Name: Your first name isn't formatted correctly, if your name uses punctuation please don't enter it and try again\n");
 	}
-	if(last == "")
+	if(mid == "")
 	{
-		result = result.concat("Last Name: You didn't put anything for your last name\n");
+		result = result.concat("Middle Initial: You didn't put anything for your middle Initial\n");
 	}
-	else if(!last.match(reg))
+	else if(!mid.match(regm))
 	{
-		result = result.concat("Last Name: Your last name isn't formatted correctly, if your name uses punctuation please don't enter it and try again\n");
+		result = result.concat("Middle Initial: Your middle Initial isn't formatted correctly\n");
 	}
 	return result;
 }
